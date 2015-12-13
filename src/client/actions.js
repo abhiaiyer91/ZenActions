19a88d75e0e773197ef83d62222f99f75f7843cc
Guilherme Decampo <guilherme.decampo@gmail.com>
2015-12-13 19:41:55 -0200

    Start factory style

--- src/client/actions.js
+++ src/client/actions.js
@@ -1,38 +1,44 @@
 /**
  * Mixins namespace to mix into our action creation methods
- * Registers a mixin by name and object containing methods to mixins (functions).
+ * Registers a mixin by name and object containing
+ * methods to mixins (functions).
  * @type {{}}
  */
 ZenMixins = {
   registerMixin(name, object) {
     if (!_.isUndefined(this[name])) {
-      throw new Meteor.Error(400, 'A Mixin with this name has already been registered');
+      throw new Meteor.Error(400,
+        'A Mixin with this name has already been registered');
     }
-    return this[name] = object;
+    this[name] = object;
+    return;
   }
 };
 
 
 /**
  * Create an Action Creator
- * @param mixins [] An array of mixin keys to attach to the object during construction
+ * @param mixins [] An array of mixin keys to attach to the
+ * object during construction
  * @constructor
  */
-ZenAction = class ActionCreator {
-  constructor(mixins) {
-    // check if the mixin array is an array of strings
-    Match.Optional([String]);
-    if (_.isEmpty(mixins)) {
-      return;
-    }
-    //iterate through the mixins
-    _.each(mixins, (item) => {
-      // grab the item from the mixin and attach to the object
-      if (!ZenMixins[item]) {
-        throw new Meteor.Error(400, 'ERROR: No Mixin with this name');
-      }
-      _.extend(this, ZenMixins[item]);
-    })
+ZenAction = function ZenAction(...mixins) {
+  // check if the mixin array is an array of strings
+  check(mixins, Match.Optional(mixins, [String]));
+
+  if (_.isEmpty(mixins)) {
+    return;
   }
+
+  let zenMixins = {};
+
+  _.each(mixins,(mixin) => {
+    if (!ZenMixins[mixin]) {
+      throw new Meteor.Error(400, 'ERROR: No Mixin with this name');
+    }
+    _.extend(zenMixins, ZenMixins[mixin]);
+  });
+
+  return zenMixins;
 };
 
